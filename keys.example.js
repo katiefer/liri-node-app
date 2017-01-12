77961b92f457ae782ed8ea6e2cd31d0458d0629f
Kate Ferleman <k.ferleman@gmail.com>
2017-01-12 17:55:01 -0600

    Create keys.example.js

---
+++ keys.example.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'input here’,
+  consumer_secret: 'input here',
+  access_token_key: 'input here',
+  access_token_secret: 'input here',
+}
